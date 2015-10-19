360e1ca9be6cdf5ef097db2f48a82b1a0a3a86d1
Volodya Esk <vkhatianovskyi@gmail.com>
2015-10-19 10:57:37 +0300

    Add unittest module, create test suite

---
+++ test/testsuite1.py
@@ -0,0 +1,14 @@
+import unittest
+from first_test_with_unitest import WaitForElements
+from assertTitle import AssertTitle
+
+class TestSuite1(unittest.TestSuite):
+
+	def suite():
+		suite = unittest.TestSuite1()
+		suite.addTest(WaitForElements('test_WaitForCheckOutPhotosButton'))
+		suite.addTest(WaitForElements('test_WaitForSearchField'))
+		suite.addTest(WaitForElements('test_AssertTitle'))
+		return suite
+	if __name__ == '__main__':
+		unittest.main()
